a9d55113732e4c70c41a06f06c19ef8f2c522dc1
sanlion <sanlion@vip.qq.com>
2016-10-18 13:48:11 +0800

    增加获取domain object的测试方法

--- ObjViewer/objView.go
+++ ObjViewer/objView.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"com.izuanqian/ObjViewer/db"
 	"com.izuanqian/ObjViewer/handler"
+	"encoding/json"
+	"fmt"
 )
 
 type eventHandler interface {
 	Do()
 }
 
+type User struct {
+	Name    string `json:"name"`
+	Age     int `json:"age"`
+	Address string `json:"address"`
+}
+
 func main() {
-
+	var user User
+	WrapViewObj("USER", "0001", &user)
+	fmt.Println(user)
 	doHandler()
-
 }
 
 func doHandler() {
@@ -21,4 +31,19 @@ func doHandler() {
 		go handler.BLPOPB()
 	}
 	<-c
+}
+
+// 从redis加载object
+// 将对象赋值到viewObj
+func WrapViewObj(key string, objSymbol string, viewObj interface{}) {
+
+	reply := db.GetOptConnection().Cmd("GET", key+":"+objSymbol)
+	objJson, _ := reply.Str()
+	if len(objJson) != 0 {
+		err := json.Unmarshal([]byte(objJson), viewObj)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
 }
